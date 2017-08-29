2a83524dd9c2747703ff445ae3374cfcbb689037
Arkady <arkady.hramov@ihousedesign.com>
2017-08-29 19:33:05 +0700

    only index.php

--- index.php
+++ index.php
@@ -3,6 +3,38 @@ spl_autoload_register(function ($class_name) {
     include 'classes/'.$class_name . '.class.php';
 });
 
+if ($_GET['url']) {
+
+    $user_url = filter_var($_GET['url'], FILTER_SANITIZE_STRING);
+
+    $host = "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['SERVER_NAME'].dirname($_SERVER['REQUEST_URI'])."/";
+
+
+    if (!filter_var($user_url, FILTER_VALIDATE_URL)){
+
+        echo "this is not a valid URL";
+
+        exit;
+
+    }
+
+    $shortUrl = new ShortUrl();
+
+
+    if( $shortUrl->shortUrlExist($user_url)) {
+
+         echo json_encode(['status'=>1,'url'=>$host.($shortUrl->getShortUrl($user_url))]);
+
+         exit;
+    }
+
+    $short_url = $shortUrl->storeUrl($user_url);
+
+    echo json_encode(['status'=>1,'url'=>$host.$short_url]);
+
+    exit;
+
+}
 
 $key = $_SERVER["QUERY_STRING"];
 
